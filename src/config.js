4aaa12cf5395498c06b2fefbd620e42324485342
NFreak <tomorrow-nf@users.noreply.github.com>
2018-09-04 12:22:43 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,8 +1,9 @@
 /*
 	config.js
 
-	Handles Gifkin configuration on startup -- will alert the user to missing files, create them if necessary, etc.
+	Handles CGCC Bot configuration on startup -- will alert the user to missing files, create them if necessary, etc.
 	(C) Adam Gincel 2018
+	Modified by Tyler "NFreak" Morrow for the CGCC Discord Server.
 */
 
 const fs = require("fs");
@@ -38,17 +39,10 @@ function configure() {
 		console.log("./info/censorshipInfo.json and .csv created.");
 	}
 
-	if (!fs.existsSync("./info/cotw.json")) {
-		didConfigure = true;
-		console.log("Cotw file not found. Contains all users' cotw submissions until cleared.");
-		fs.writeFileSync("./info/cotw.json", "{}", "utf8");
-		console.log("./info/cotw.json created.");
-	}
-
 	if (!fs.existsSync("./info/discordToken.txt")) {
 		didConfigure = true;
 		console.log("Discord Token text file not found. This file should contain your Discord Bot token.");
-		fs.writeFileSync("./info/discordToken.txt", "Yah2hAtenajce0ay342CA.ABC123.esUgasemrawfa_Pc9Q", "utf8");
+		fs.writeFileSync("./info/discordToken.txt", "PutYour.Token_Here", "utf8");
 		console.log("./info/discordToken.txt created and filled with example token.");
 	}
 
@@ -58,52 +52,28 @@ function configure() {
 			roles: "123456",
 			secondary: "654321",
 			announcements: "3123131",
-			patchnotes: "71563715631"
 		};
 		console.log("ids file not found. This file contains the specific IDs of either messages or channels that are used for things like reading specific message reactions, posting to specific server channels, etc.");
 		fs.writeFileSync("./info/ids.json", JSON.stringify(ids), "utf8");
 		console.log("./info/ids.json created and populated with example data.");
 	}
 
-	if (!fs.existsSync("./info/recent-news.txt")) {
-		didConfigure = true;
-		fs.writeFileSync("./info/recent-news.txt", "", "utf8");
-		console.log("./info/recent-news.txt created. This tracks the latest Icons news link. Unused as of time of writing.");
-	}
-
 	if (!fs.existsSync("./info/roleEmoji.json")) {
 		didConfigure = true;
 		fs.writeFileSync("./info/roleEmoji.json", "[]", "utf8");
-		console.log("./info/roleEmoji.json created. This list is what gets added to any message in the set-your-roles channel after it sees a :wavedash: react. Configured with the =emotelist command.");
+		console.log("./info/roleEmoji.json created. This list is what gets added to any message in the role-assignment channel after it sees a :zhuW: react. Configured with the !emotelist command.");
 	}
 
 	if (!fs.existsSync("./info/todo.json")) {
 		didConfigure = true;
 		fs.writeFileSync("./info/todo.json", "[]", "utf8");
-		console.log("./info/todo.json created. This is a todolist, modified with =todo.");
+		console.log("./info/todo.json created. This is a todolist, modified with !todo.");
 	}
 
 	if (!fs.existsSync("./info/userCommands.json")) {
 		didConfigure = true;
 		fs.writeFileSync("./info/userCommands.json", "[]", "utf8");
-		console.log("./info/userCommands.json created. This is the list of dynamically created call-and-response commands for all users, managed with =setcommand, =describecommand, and =deletecommand");
-	}
-
-	if (!fs.existsSync("./info/voicechannels.json")) {
-		didConfigure = true;
-		let vc = {
-			category: "123456789",
-			default: [
-				"123456789"
-			],
-			dynamic: [
-				{name: "Dynamic Voice 1", id: null},
-				{name: "Dynamic Voice 2", id: null},
-				{name: "Final Dynamic Voice", id: null}
-			]
-		};
-		fs.writeFileSync("./info/voicechannels.json", JSON.stringify(vc), "utf8");
-		console.log("./info/voicechannels.json created. This contains the category ID where all dynamic voice channels should be placed, a list of Default voice channels that will never be destroyed, and a list of dynamic channel names with null IDs, which will be populated and de-populated at runtime.");
+		console.log("./info/userCommands.json created. This is the list of dynamically created call-and-response commands for all users, managed with !setcommand, !describecommand, and !deletecommand");
 	}
 
 	if (!fs.existsSync("./info/reminders.json")) {
@@ -111,12 +81,6 @@ function configure() {
 		fs.writeFileSync("./info/reminders.json", "[]", "utf8");
 		console.log("./info/reminders.json created. A list of objects with Dates and Messages, sent to the ids.reminders channel once the given date is passed.");
 	}	
-
-	if (!fs.existsSync("./info/feedbacks.json")) {
-		didConfigure = true;
-		fs.writeFileSync("./info/feedbacks.json", "{}", "utf8");
-		console.log("./info/feedbacks.json created. Maps feedback givers IDs (not usernames, not used to track who is submitting what feedback)'s quantity of submitted feedback messages, to put a limit on griefing.");
-	}
 
 	if (didConfigure) {
 		console.log("Configuration complete.");
