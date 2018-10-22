aa08667ac874d6cda71a9a787dc29c02c9953a90
Tyler Morrow <nfreakdq@gmail.com>
2018-10-22 19:36:06 +0000

    Updating spambot stuff

--- src/commands.js
+++ src/commands.js
@@ -23,19 +23,19 @@ let helpString = ["", ""];
 
 helpString[0] += "`!members` - Tell us how many members are on a server, and how many are online.\n";
 helpString[0] += "`!top PLACEMENTS NUM_MESSAGES CHANNEL_NAME` - Tells you the top `PLACEMENTS` most frequent posters over the last `NUM_MESSAGES` messages in #`CHANNEL_NAME` (more messages = more time)\n";
-helpString[0] += "`!setcommand COMMAND_NAME text` - Will create a user-accessible =`COMMAND_NAME` that will make Gifkin return any text after `COMMAND_NAME`.\n";
+helpString[0] += "`!setcommand COMMAND_NAME text` - Will create a user-accessible =`COMMAND_NAME` that will make the bot return any text after `COMMAND_NAME`.\n";
 helpString[0] += "`!describecommand COMMAND_NAME description` - Adds a description to display in `!help` for the users.\n";
 helpString[0] += "`!removecommand COMMAND_NAME` - Will remove the user-accessible =`COMMAND_NAME`, if it exists.\n";
 helpString[0] += "`!hidecommand COMMAND_NAME` - Toggles visibility of a help command.\n";
 helpString[0] += "`!helpcount` - Show number of uses each user command has recieved.\n";
 helpString[0] += "`!helphidden` - Display hidden user commands.\n";
 helpString[0] += "`!kill` - End this bot instance. Bot should automatically restart.\n";
-helpString[0] += "`!refresh` - Remove all reacts not by Gifkin in the roles channel.\n";
+helpString[0] += "`!refresh` - Remove all reacts not by the bot in the roles channel.\n";
 helpString[0] += "`!say CHANNEL MESSAGE` - Send any message to any channel.\n";
 helpString[0] += "`!purge CHANNEL NUMBER` - Delete NUMBER messages from CHANNEL.\n";
 helpString[0] += "`!remindme DAYS MESSAGE` - Send an automatic message to the bot-spam channel after `DAYS` days have passed.\n";
 
-helpString[1] += "`!emotelist EMOTES` - The list of emotes to add to a message when reacting with :wavedash:.\n";
+helpString[1] += "`!emotelist EMOTES` - The list of emotes to add to a message when reacting with :cgccWhite:.\n";
 helpString[1] += "`!todo` - Display the todo list.\n";
 helpString[1] += "`!todo add task` - Adds `task` to the todo list.\n";
 helpString[1] += "`!todo remove task` - Removes `task` from the todo list. Either by string or number.\n";
@@ -419,7 +419,7 @@ async function modCommands(message, args) {
 		else if (args.length > 1 && args[1] == "add") {
 			if (args.length > 2) {
 				spamlist.push(args[2]);
-				fs.writeFileSync('./info/spamlist.json', JSON.stringify(spamlist), 'utf8');
+				fs.writeFileSync('./info/spam.json', JSON.stringify(spamlist), 'utf8');
 				await message.channel.send("`" + args[2] + "` has been added to the spamlist.");
 			} else {
 				await message.channel.send("Usage: `!spamlist add word`");
@@ -429,7 +429,7 @@ async function modCommands(message, args) {
 				let ind = spamlist.indexOf(args[2]);
 				if (ind > -1) {
 					spamlist.splice(ind, 1);
-					fs.writeFileSync('./info/spamlist.json', JSON.stringify(spamlist), 'utf8');
+					fs.writeFileSync('./info/spam.json', JSON.stringify(spamlist), 'utf8');
 					await message.channel.send("`" + args[2] + "` has been removed from the spamlist.");
 				} else {
 					await message.channel.send("`" + args[2] + "` was not found in the spamlist.");
