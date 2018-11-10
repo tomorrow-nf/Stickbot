db9fae0ef760316344786916ee08d32f349e4023
Tyler Morrow <nfreakdq@gmail.com>
2018-11-10 14:00:46 -0500

    Adding robust challenge commands

--- src/commands.js
+++ src/commands.js
@@ -461,8 +461,8 @@ async function userCommands(message, args) {
 		var currentDate = new Date();
 		var submissionStart = new Date('November 10, 2018 12:00 GMT-04:00');
 		var submissionDeadline = new Date('November 11, 2018 23:59 GMT-04:00');
-		var voteStart = new Date('November 12, 2018 12:00 GMT-04:00');
-		var voteEnd = new Date('November 14, 2018 12:00 GMT-04:00');
+		var voteStart = new Date('November 12, 2018 00:00 GMT-04:00');
+		var voteEnd = new Date('November 14, 2018 00:00 GMT-04:00');
 
 		if (args[1] == "help" || args.length < 2 ||
 			(args[1] != "submit" && args[1] != "vote" && args[1] != "view")) {
