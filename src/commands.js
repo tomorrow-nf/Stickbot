f57ac996876e9dd51108949059837341ad6b515c
Tyler Morrow <nfreakdq@gmail.com>
2018-11-12 06:26:43 -0500

    Adding robust challenge commands

--- src/commands.js
+++ src/commands.js
@@ -471,11 +471,11 @@ async function userCommands(message, args) {
 																		   "\n`!challenge vote ENTRY-NUMBER`: Vote for the provided entry" + 
 																		   "\n`!challenge view`: View all submissions (must have DMs enabled on this server)" +
 																		   "\n`!challenge help`: Display this message\n" +
-											 "\n Challenge submissions open: `" + submissionStart.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT`" +
-											 "\n Submission deadline: `" + submissionDeadline.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT`" +
-											 "\n Voting begins: `" + voteStart.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT`" +
-											 "\n Voting deadline: `" + voteEnd.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT`" + 
-											 "\n For reference, the current time is: " + currentDate.toLocaleString('en-US', { timeZone: "America/Montserrat" }));
+											 "\n Challenge submissions open: `" + submissionStart.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT`" +
+											 "\n Submission deadline: `" + submissionDeadline.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT`" +
+											 "\n Voting begins: `" + voteStart.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT`" +
+											 "\n Voting deadline: `" + voteEnd.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT`" + 
+											 "\n For reference, the current time is: `" + currentDate.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT`");
 		} 
 		else if (args[1] == "submit"){
 			if (message.channel.type != "dm"){
@@ -485,7 +485,7 @@ async function userCommands(message, args) {
 				return await message.channel.send("USAGE: `!challenge submit LINK-TO-ENTRY DESCRIPTION`, where 'LINK-TO-ENTRY' is an imgur/google drive/etc link, and 'DESCRIPTION' is a title or writeup on your entry.");
 			} 
 			if (currentDate < submissionStart){
-				return await message.channel.send("Challenge submissions are not yet open. Submit your entry after: " + submissionStart.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT");
+				return await message.channel.send("Challenge submissions are not yet open. Submit your entry after: " + submissionStart.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT");
 			}
 			if (currentDate > submissionDeadline){
 				return await message.channel.send("Challenge submissions are now closed, sorry!");
@@ -526,7 +526,7 @@ async function userCommands(message, args) {
 				return await message.channel.send("USAGE: `!challenge vote ENTRY-NUMBER`");
 			} 
 			if (currentDate < voteStart){
-				return await message.channel.send("Challenge voting is not yet open. Voting will open on: " + voteStart.toLocaleString('en-US', { timeZone: "America/Montserrat" }) + " EDT");
+				return await message.channel.send("Challenge voting is not yet open. Voting will open on: " + voteStart.toLocaleString('en-US', { timeZone: "America/New_York" }) + " EDT");
 			}
 			if (currentDate > voteEnd){
 				return await message.channel.send("Challenge voting has closed, sorry! Check back for the next challenge");
